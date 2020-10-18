416c7352ae62f1e3e0d43badac96ee001fa42788
astherath <farceriv@gmail.com>
2020-10-18 13:40:04 -0400

    middle of debugging

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,42 @@
 pub mod cli {
-    use std::env;
+    extern crate clap;
+    use clap::{App, Arg};
 
-    pub fn get_args() {
-        let pattern = env::args().nth(1).expect("no arg found on pos 1");
-        println!("{}", pattern);
-    }
+    pub fn get_matches() {
+        let matches = App::new("rustler")
+            .version("1.0")
+            .about("Rustles files for TODO and FIXME comments")
+            // .arg(
+            // Arg::with_name("config")
+            // .short("c")
+            // .long("config")
+            // .value_name("FILE")
+            // .help("Sets a custom config file")
+            // .takes_value(true),
+            // )
+            .arg(
+                Arg::with_name("filename")
+                    .help("Sets the input file to rustle")
+                    .required(true)
+                    .index(1),
+            )
+            // .arg(
+            // Arg::with_name("v")
+            // .short("v")
+            // .multiple(true)
+            // .help("Sets the level of verbosity"),
+            // )
+            .get_matches();
 
-    pub struct Cli {
-        filename: std::
+        // Gets a value for config if supplied by user, or defaults to "default.conf"
+        let config = matches.value_of("config").unwrap_or("default.conf");
+        println!("Value for config: {}", config);
+
+        // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+        // required we could have used an 'if let' to conditionally get the value)
+        println!(
+            "Using input file: {}",
+            matches.value_of("filename").unwrap()
+        );
     }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod cli;
 mod parser;
 
 fn main() {
-    cli::cli::get_args();
-    let filename = String::from("example.txt");
-    parser::parser::read_file_data(filename);
+    cli::cli::get_matches();
+    // let filename = String::from("example.txt");
+    // parser::parser::read_file_data(filename);
 }
