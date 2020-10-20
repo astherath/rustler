cf35cf254ed62288103a44f351a1ca4f1ce71e59
astherath <farceriv@gmail.com>
2020-10-19 22:00:26 -0400

    added more functionality to the lines mod and now the parser only includes non-empty lines

--- src/lines.rs
+++ src/lines.rs
@@ -1,4 +1,5 @@
 pub mod lines {
+    use std::cmp;
 
     pub struct CodePatch {
         pub line: String,
@@ -7,6 +8,55 @@ pub mod lines {
     impl CodePatch {
         pub fn new(line: String) -> CodePatch {
             CodePatch { line }
+        }
+
+        fn check_line_special(line: &String) -> bool {
+            line.to_lowercase().contains("todo")
+        }
+
+        // fn print_all_todos(&self) {}
+
+        /// Reads the file line-by-line into a context-driven `lines::CodePatch` struct
+        pub fn unpack_lines_to_code_patch_vec(
+            lines: Vec<String>,
+            context: usize,
+        ) -> Vec<CodePatch> {
+            let mut code_patch_vec = Vec::new();
+            let mut i = 0;
+            let lines_len = lines.len();
+            while i < lines_len {
+                if CodePatch::check_line_special(&lines[i]) {
+                    let back_range = i.checked_sub(context).unwrap_or(0);
+                    let mut full_context = String::new();
+
+                    // add context # of lines behind
+                    for j in back_range..i {
+                        full_context.push_str(&lines[j].trim());
+                        full_context.push_str("\n");
+                    }
+
+                    // add current line
+                    full_context.push_str(&lines[i].trim());
+                    full_context.push_str("\n");
+
+                    // add context # of line ahead
+                    let front_range = cmp::min(lines_len, i + context) + 1;
+                    for j in (i + 1)..front_range {
+                        full_context.push_str(&lines[j].trim());
+                        full_context.push_str("\n");
+                    }
+                    println!("{}------------------------------", &full_context);
+                    // XXX: debug
+                }
+                i += 1;
+            }
+            code_patch_vec
+        }
+
+        pub fn print_if_special(&self) {
+            if CodePatch::check_line_special(&self.line) {
+                println!("{}", &self.line);
+            }
         }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -12,25 +12,18 @@ pub mod parser {
     }
 
     // /// Top-level function to mask the usage of the FileParser for a simple interface. Returns
-    // ///
-    // ///
-    // ///
-    // ///
     pub fn read_file_data(filename: String) {
         let file_op = FileParser::new(filename);
-        // file_op.print_all_lines()
-        let resp = file_op.unpack_lines_to_code_patch();
-        let mut i = 1;
-        for l in resp {
-            println!("line #{} is: {}", i, l.line);
-            i += 1;
+        let code_patch_vec = CodePatch::unpack_lines_to_code_patch_vec(file_op.lines, 2);
+
+        for code_patch in code_patch_vec {
+            code_patch.print_if_special();
         }
     }
 
     struct FileParser {
         filename: String,
         lines: Vec<String>,
-        current_line: u32,
     }
 
     impl FileParser {
@@ -42,18 +35,14 @@ pub mod parser {
         ///
         /// # Notes
         ///
-        /// The filename is NOT checked to exist at this point. However the file will be checked for completion on read so no double check needed.
+        /// The filename is NOT checked to exist at this point.
+        /// However the file will be checked for completion on read so no double check needed.
         fn new(filename: String) -> FileParser {
             let lines = FileParser::get_lines_from_file(&filename);
-            let response = FileParser {
-                filename,
-                lines,
-                current_line: 1,
-            };
-            response
+            FileParser { filename, lines }
         }
 
-        /// Returns a line iterator for the given filename
+        /// Reads and returns an iterator with the line data for the given filename
         ///
         /// # Arguments
         ///
@@ -61,7 +50,7 @@ pub mod parser {
         ///
         /// # Returns
         ///
-        /// `io::Lines` iterator from the file
+        /// `Vec<String>` of all of the lines in the file
         fn get_lines_from_file(filename: &String) -> Vec<String> {
             let path = Path::new(filename);
             let file = match File::open(&path) {
@@ -72,44 +61,22 @@ pub mod parser {
                 ),
                 Ok(file) => file,
             };
+
+            // unpack line iterator into vector
             let lines_iter = io::BufReader::new(file).lines();
             let mut lines = Vec::new();
 
-            // unpack line iterator to vector
             for line_result in lines_iter {
                 match line_result {
                     Err(why) => panic!("cannot read line {}", Colour::Red.paint(why.to_string())),
-                    Ok(line) => lines.push(line),
+                    Ok(line) => {
+                        if !line.is_empty() {
+                            lines.push(line)
+                        }
+                    }
                 }
             }
             lines
         }
-
-        /// Reads the file line-by-line into a context-driven `lines::CodePatch` struct
-        fn unpack_lines_to_code_patch(&self) -> Vec<CodePatch> {
-            let lines = &self.lines;
-            let mut code_patch_vec = Vec::new();
-            for line in lines {
-                code_patch_vec.push(CodePatch::new(line.to_string()));
-                // match line_result {
-                // Err(why) => panic!("cannot read line {}", Colour::Red.paint(why.to_string())),
-                // Ok(line) => code_patch_vec.push(CodePatch::new(line)),
-                // }
-            }
-            code_patch_vec
-        }
-
-        fn check_line_todo(line: &String) -> bool {
-            line.to_lowercase().contains("todo")
-        }
-
-        // fn inc_line_number(&mut self) {
-        // self.current_line += 1
-        // }
-
-        // fn process_special_line(&mut self, line: &String) {
-        // self.inc_special_line_count();
-        // self.add_special_line();
-        // }
     }
 }
